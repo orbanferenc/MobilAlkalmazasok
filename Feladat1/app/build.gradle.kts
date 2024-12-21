3d6c097795b2a6801673ed525e30dc78f2123846
Orban Erika <erika.orban@andreasoft.ro>
2024-12-21 22:07:48 +0200

    Made the project buildable.

--- Feladat1/app/build.gradle.kts
+++ Feladat1/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.feladat1"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.feladat1"
--- Feladat1/settings.gradle.kts
+++ Feladat1/settings.gradle.kts
@@ -21,4 +21,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "Feladat1"
 include(":app")
- 
+include(":app")
